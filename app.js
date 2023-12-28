079c104b78880c40c8b2aeecdd67cc079ec4a3b9
GhulamHaider79 <127304370+GhulamHaider79@users.noreply.github.com>
2023-12-28 21:44:17 +0500

    reponsive

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
+var endDate = new Date("March 10, 2024 23:59:59").getTime();
+var newDate = new Date().getTime();
+var diffDate = endDate - newDate;
 
+function ramadanCounter() {
 
-
- var endDate = new Date("March 10, 2024 23:59:59").getTime()
- console.log(endDate);
-
- var newDate = new Date().getTime();
- console.log(newDate);
-
- var diffDate =  endDate - newDate;
-
- console.log(diffDate);
-
-     function ramadanCounter (){
-
-    var month = Math.floor (diffDate / (1000 * 60 * 60 * 24 * 30));
-    var day = Math.floor (diffDate % (1000 * 60 * 60 * 24 * 30)/(1000 * 60 * 60 * 24));
-    var hours = Math.floor (diffDate % (1000 * 60 * 60 * 24)/(1000 * 60 * 60 ));
-    var minute = Math.floor (diffDate % (1000 * 60 * 60)/(1000 * 60));
-    var second = Math.floor (diffDate % (1000 * 60)/(1000));
-    console.log(second);
-
-   var counter = document.querySelector('.month').innerText = month;
-   var counter = document.querySelector('.day').innerText = day;
-   var counter = document.querySelector('.hours').innerText = hours;
-   var counter = document.querySelector('.minute').innerText = minute;
-   var counter = document.querySelector('.second').innerText = second;
+    var month = Math.floor(diffDate / (1000 * 60 * 60 * 24 * 30));
+    var day = Math.floor(diffDate % (1000 * 60 * 60 * 24 * 30) / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(diffDate % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minute = Math.floor(diffDate % (1000 * 60 * 60) / (1000 * 60));
+    var second = Math.floor(diffDate % (1000 * 60) / (1000));
+    var counter = document.querySelector('.month').innerText = month;
+    var counter = document.querySelector('.day').innerText = day;
+    var counter = document.querySelector('.hours').innerText = hours;
+    var counter = document.querySelector('.minute').innerText = minute;
+    var counter = document.querySelector('.second').innerText = second;
 }
 
 
- setInterval(ramadanCounter, 1000);
+setInterval(ramadanCounter, 1000);
 
- ramadanCounter ()
-
-
- 
-
-
-
-
- 
+ramadanCounter()
